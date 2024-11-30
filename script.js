fd5f423b4d624ca75d41ff27d18545032ed8ef4d
Paulo <67171671+FakeDelhiGuy@users.noreply.github.com>
2024-11-29 23:35:01 -0300

    Opções atualizadas

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
         navMenu.classList.toggle("active");
     });
 });
+function openFullScreen(imgElement) {
+    const fullscreen = document.getElementById("fullscreen");
+    const fullscreenImg = document.getElementById("fullscreen-img");
+
+    // Define a imagem clicada como a imagem em tela cheia
+    fullscreenImg.src = imgElement.src;
+    fullscreen.style.display = "flex";
+}
+
+function closeFullScreen() {
+    const fullscreen = document.getElementById("fullscreen");
+
+    // Esconde o modo tela cheia
+    fullscreen.style.display = "none";
+}
